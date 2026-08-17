b10688d0b15354974bb649f641f40d456723833e
Fabricio Mdec <fabriciomdec@gmail.com>
2026-08-17 20:27:26 -0300

    chore(config): remove unused path aliases from tsconfig and vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@lib': fileURLToPath(new URL('./src/lib', import.meta.url))
     }
   }
 })
