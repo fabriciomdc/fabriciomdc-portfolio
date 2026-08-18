621781a6036ed4feb26ac13ea2654af4f578dee9
Fabricio Mdec <fabriciomdec@gmail.com>
2026-08-18 14:38:15 -0300

    feat: add cv to project

--- src/presentation/main/routes/index.tsx
+++ src/presentation/main/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Navbar } from '@/presentation/components/layout/navbar'
 import { SectionAnimation } from '@/presentation/components/ui/section-animation'
 import { Home } from '@/presentation/pages/home'
@@ -6,15 +7,22 @@ import { Skills } from '@/presentation/pages/skills'
 import { Projects } from '@/presentation/pages/projects'
 import { Experiences } from '@/presentation/pages/experiences'
 import { Contact } from '@/presentation/pages/contact'
+import { CVPreview } from '@/presentation/pages/cv-preview'
 
 export const Layout = () => {
+  const [showCV, setShowCV] = useState(false)
+
+  if (showCV) {
+    return <CVPreview onBack={() => setShowCV(false)} />
+  }
+
   return (
     <>
       <Navbar />
       <main className="pt-28 paper-texture">
         <div className="mx-auto w-[95vw] lg:w-[85vw] border-x-2 border-foreground">
           <SectionAnimation variant="fadeIn">
-            <Home />
+            <Home onOpenCV={() => setShowCV(true)} />
           </SectionAnimation>
           <SectionAnimation variant="fadeInUp">
             <About />
